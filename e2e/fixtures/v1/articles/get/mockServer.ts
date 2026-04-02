e63fb2586e4a62a088759ab23f16fb4db6695974
hayatohanaoka <suzugamori.hh@gmail.com>
2026-04-02 23:47:37 +0900

    e2e: Add fixture

--- e2e/fixtures/v1/articles/get/mockServer.ts
+++ e2e/fixtures/v1/articles/get/mockServer.ts
@@ -1,11 +1,15 @@
 import { http, HttpResponse } from 'msw'
 import { setupServer } from 'msw/node'
-import qiitaArticlesJson from './qiita.json' with { type: 'json' }
+import qiitaArticlesJson from './mappings/qiita.json' with { type: 'json' }
+import zennArticlesJson from './mappings/zenn.json' with { type: 'json' }
 
 export const handlers = [
   http.get('https://qiita.com/api/v2/items?per_page=100', () => {
     return HttpResponse.json(qiitaArticlesJson)
   }),
+  http.get('https://zenn.dev/api/articles?article_type=tech', () => {
+    return HttpResponse.json(zennArticlesJson)
+  })
 ]
 
 export const server = setupServer(...handlers)
