8f3914e3a061deabf9a17e8f454d3e420a7eba95
hayatohanaoka <suzugamori.hh@gmail.com>
2026-05-05 09:59:49 +0900

    app: change abstract class -> interface

--- app/src/main/port/articlesPort.ts
+++ app/src/main/port/articlesPort.ts
@@ -1,7 +1,7 @@
 import type { Articles } from "../domain/articles.js";
 import type { QueryParams } from "../domain/queries.js";
 
-export abstract class ArticlesPort {
-    abstract get(): Promise<Articles>
-    abstract search(queryParams: QueryParams): Promise<Articles>
+export interface ArticlesPort {
+    get(): Promise<Articles>
+    search(queryParams: QueryParams): Promise<Articles>
 }
